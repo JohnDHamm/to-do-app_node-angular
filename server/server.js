9d0e8316502eb39d4d30831b6043f0c6cef98897
John Hamm <johndhamm1@gmail.com>
2016-09-26 14:40:47 -0500

    basic angular structure - fixed

--- server/server.js
+++ server/server.js
@@ -9,9 +9,4 @@ const app = express();
 //middlewares
 app.use(express.static('client')) //client folder
 
-
-app.get('/api/title', (req, res) =>
-	res.send({ title: 'DO IT ALREADY' })
-)
-
 app.listen(PORT, () => console.log(`listening on port: ${PORT}`))
